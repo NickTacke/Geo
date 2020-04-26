038ec769123ddc8fd26a43bf68ed152b30abd44f
Nick Tacke <44647382+NickTacke@users.noreply.github.com>
2020-04-26 15:11:11 +0200

    Update ScriptList.lua

--- ScriptList.lua
+++ ScriptList.lua
@@ -1,3 +1,3 @@
 return {
-  ["73885730"] = ["https://raw.githubusercontent.com/NickTacke/Geo/master/Scripts/PrisonLife.lua?token=AKUUHVQHLB4XNSSTBJ76XSS6V26IG"]
+  ["73885730"] = ["https://raw.githubusercontent.com/NickTacke/Geo/master/Scripts/PrisonLife.lua?token=AKUUHVVOJG5NJWSZMZOOLDS6V3BNW"]
 }
